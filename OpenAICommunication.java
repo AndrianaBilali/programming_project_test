1dd645b7db81b49318e49dde0798d3ecf070dfc0
Andriana <t8220100@aueb.gr>
2023-11-16 12:29:43 +0200

    Structure method recieveResponse

--- OpenAICommunication.java
+++ OpenAICommunication.java
@@ -4,16 +4,23 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import org.json.JSONObject; //add dependency on maven
 import java.io.OutputStream;
-import java.net.URI;
 
 public class OpenAICommunication {
     // setting the API endpoint
-    private final URL url = new URL("https://api.openai.com/v1/chat/completions");
-    private HttpURLConnection connection = (HttpURLConnection) null;
+    private static final String endpoint = "https://api.openai.com/v1/chat/completions";
+    HttpURLConnection connection = null;
 
     public void openConnection() {
+        URL url = null;
+        try {
+            url = new URL(endpoint);
+        } catch (MalformedURLException e) {
+            System.err.println("Invalid url: " + e);
+            System.exit(1);
+        }
         try {
             connection = (HttpURLConnection) url.openConnection(); // openning connection
         } catch (ClassCastException e) {
@@ -28,7 +35,7 @@ public class OpenAICommunication {
         }
     }
 
-    public void sendRequest(String userQuestion, HttpURLConnection connection) {
+    public void sendRequest(String userQuestion) {
         connection.setRequestMethod("POST"); // indicating that i will be sending data to the model
         connection.setRequestProperty("Content-Type", "application/json");
         connection.setRequestProperty("Authorization", "Bearer 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"); // openai
@@ -37,11 +44,17 @@ public class OpenAICommunication {
         jo.put("q: ", userQuestion); // passing the user's question into the json object
         String inputJSON = jo.toString(); // finally converting the json object to a string
 
+        connection.setDoInput(true); // to indicate that i am going to give data
         connection.setDoOutput(true); // to indicate that i am expecting the model's response
 
         OutputStream obj = connection.getOutputStream(); // creating an object
         byte[] array = inputJSON.getBytes("utf-8"); // interpreting the text to bytes
-        obj.write(array, 0, array.length); // writing all the bytes to the stream
+        try {
+            obj.write(array, 0, array.length); // writing all the bytes to the stream
+            obj.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // At this point we have made the connection to the server and sent the user's
         // request.
@@ -49,7 +62,30 @@ public class OpenAICommunication {
     }
 
     // method to recieve response
-    public void recieveResponse(HttpURLConnection connection) {
+    public void recieveResponse() {
+        try {
+            connection.setRequestMethod("GET");
+        } catch (ProtocolException e) {
+            System.err.println("Protocol Exception: " + e);
+            System.exit(1);
+        }
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        } catch (IOException e) {
+            System.err.println("Connection error: " + e);
+            System.exit(1);
+        }
 
+        int c;
+        StringBuilder response = new StringBuilder();
+        try {
+            while ((c = in.read()) != -1) {
+                response.append((char) c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(response.toString());
     }
 }
