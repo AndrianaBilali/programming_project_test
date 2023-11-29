746098023de8f3775b6394759b77dcda30cd68b6
EleniKaspiri <146988852+EleniKaspiri@users.noreply.github.com>
2023-11-29 15:57:14 +0200

    Add files via upload

---
+++ Preferences.java
@@ -0,0 +1,42 @@
+package com.foodapp.food_app;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Preferences {
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long preferenceId;
+
+    @Column(nullable = false)
+    private String allergy;
+
+    // Constructors
+
+    // Default constructor
+    public Preferences() {
+    }
+
+    // Parameterized constructor
+    public Preferences(String allergy) {
+        this.allergy = allergy;
+    }
+
+    // Getters and Setters
+    public Long getPreferenceId() {
+        return preferenceId;
+    }
+
+    public void setAllergy(String allergy) {
+        this.allergy = allergy;
+    }
+
+    public String getAllergy() {
+        return allergy;
+    }
+}
