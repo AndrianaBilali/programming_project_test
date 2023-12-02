47985e84b5f634542a456f8a2df91e3c105203fc
t8220128 <147388357+t8220128@users.noreply.github.com>
2023-12-02 13:52:56 +0200

    Update UserQuery.java
    -addition of a method for checking if the query is written in English.                    
    -addition of  a method for checking the existence of inappropriate content in the query
    -package declaration
    -modification of some methods

--- UserQuery.java
+++ UserQuery.java
@@ -1,12 +1,24 @@
-
+package com.example;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
 import org.apache.commons.lang3.StringUtils;
-
+import java.util.Scanner;
 
 public class UserQuery {
     private String query ;
+    String apiKey = "AIzaSyCIvPSelJS85H7dnqWxaRd1GOD9wNBZ5dc";
+    Scanner input = new Scanner(System.in); // declaration and initializtion of a Scanner object
     // receives the user's query 
-    UserQuery() {
-        query = "";
+    UserQuery(String query) {
+        this.query = query;
+    }
+    //setters and getters
+    public void setQuery(String query){
+        this.query = query;
+    }
+     public String getQuery(){
+        return query;
     }
     //removes the blank spaces from the query and returns it
     public String TrimQuery () {
@@ -24,7 +36,8 @@ public class UserQuery {
     public String EmptyQuery() {
         while (query.isEmpty() == true) {
             System.out.println("You have not entered a query. Please try again");
-            query = "";
+            String newQuery = input.nextLine(); //the user enters the query again
+            query = newQuery;// place the new query in the query variable
             }
             return query;
         }
@@ -32,6 +45,31 @@ public class UserQuery {
     public String remove_Newlines_Tabs() {
         query = query.replaceAll("[\n\t]", "");
         return query;
+    }
+    //checks if the query is written in the English language
+    public String checkLanguage() {
+        while (not (query.matches("[a-zA-Z]+"))) {
+                System.out.println("The query doesn't contain latin characters. Please reenter your query in the English language");
+                String newQuery = input.nextLine(); //the user enters the query again
+                query = newQuery;// place the new query in the query variable
+        }
+        return query;
+    }
+    //checks for sensitive or inappropriate content
+    public String checkContent() {
+        HttpResponse<JsonNode> response = Unirest.post("https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=" + apiKey)
+        .header("Content-Type", "application/json")
+        .body("{\"comment\":{\"text\":\"" + query + "\"},\"languages\":[\"en\"],\"requestedAttributes\":{\"TOXICITY\":{}}}")
+        .asJson();
+
+        System.out.println(response.getBody().toPrettyString());
+        double toxicity = response.getBody().getObject().getJSNObject("attributeScores").getJSNObject("TOXICITY").getJSNObject("summaryScore").getDouble("value");
+        System.out.println("Inappropriate content" + toxicity);
+        if (toxicity >= 0.0) {
+        System.out.println("The query that you have entered contains inappropriate content. Please reenter your query");
+        String newQuery = input.nextLine(); //the user enters the query again
+        query = newQuery;// place the new query in the query variable
+        }
     }
 }
 
