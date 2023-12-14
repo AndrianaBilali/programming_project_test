a794a75d02adda90a4c1930258195b2b2cbf71eb
t8220128 <147388357+t8220128@users.noreply.github.com>
2023-12-14 23:18:32 +0200

    Update UserQuery.java
    addition of a method
    withdrawal of a method

--- UserQuery.java
+++ UserQuery.java
@@ -1,13 +1,9 @@
 package com.example;
-import kong.unirest.HttpResponse;
-import kong.unirest.JsonNode;
-import kong.unirest.Unirest;
-import org.apache.commons.lang3.StringUtils;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class UserQuery {
     private String query ;
-    String apiKey = "AIzaSyCIvPSelJS85H7dnqWxaRd1GOD9wNBZ5dc";
     Scanner input = new Scanner(System.in); // declaration and initializtion of a Scanner object
     // receives the user's query 
     UserQuery(String query) {
@@ -29,9 +25,9 @@ public class UserQuery {
         query = query.toLowerCase();
         return query;
     }// first letter capital
-    public String CapitalFirstLetter() {
-        query = StringUtils.capitalize(query);
-        return query;
+   public String CapitalFirstLetter() {
+            query = query.substring(0,1).toUpperCase() +query.substring(1);
+            return query;
     }// checks if the user has not entered a query 
     public String EmptyQuery() {
         while (query.isEmpty() == true) {
@@ -48,29 +44,21 @@ public class UserQuery {
     }
     //checks if the query is written in the English language
     public String checkLanguage() {
-        while (!query.matches("[a-zA-Z]+")) {
+        while (query.matches("[a-zA-Z ?]+") != true){
                 System.out.println("The query doesn't contain latin characters. Please reenter your query in the English language");
                 String newQuery = input.nextLine(); //the user enters the query again
                 query = newQuery;// place the new query in the query variable
         }
         return query;
     }
-    //checks for sensitive or inappropriate content
-    public String checkContent() {
-        HttpResponse<JsonNode> response = Unirest.post("https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=" + apiKey)
-        .header("Content-Type", "application/json")
-        .body("{\"comment\":{\"text\":\"" + query + "\"},\"languages\":[\"en\"],\"requestedAttributes\":{\"TOXICITY\":{}}}")
-        .asJson();
-
-        System.out.println(response.getBody().toPrettyString());
-        double toxicity = response.getBody().getObject().getJSNObject("attributeScores").getJSNObject("TOXICITY").getJSNObject("summaryScore").getDouble("value");
-        System.out.println("Inappropriate content" + toxicity);
-        if (toxicity >= 0.1) {
-        System.out.println("The query that you have entered contains inappropriate content. Please reenter your query");
-        String newQuery = input.nextLine(); //the user enters the query again
-        query = newQuery;// place the new query in the query variable
+    //checks if the query is relative with the food
+   public void foodContent() {
+        boolean containsFood = query.contains("recipy");
+        if (!containsFood) {
+            System.out.println("The query doesn't relate with food. Please try again.");
+            String s = input.nextLine();
+            this.setQuery(s);
         }
-        return query;
     }
 }
 
