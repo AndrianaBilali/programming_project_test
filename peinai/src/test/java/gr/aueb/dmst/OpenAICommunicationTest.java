7f37457034853713ecef16775fc97c84a60068a5
AndrianaBilali <148336032+AndrianaBilali@users.noreply.github.com>
2023-12-24 23:12:11 +0200

    Update OpenAICommunicationTest.java

--- peinai/src/test/java/gr/aueb/dmst/OpenAICommunicationTest.java
+++ peinai/src/test/java/gr/aueb/dmst/OpenAICommunicationTest.java
@@ -1,20 +1,33 @@
 package gr.aueb.dmst;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class OpenAICommunicationTest {
+    private OpenAICommunication openAI;
+
+    @BeforeEach
+    public void setUp() {
+        openAI = new OpenAICommunication();
+        openAI.openConnection();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        openAI.closeConnection();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testSendRequestAndReceiveResponse() {
+        String userQuestion = "Tell me a recipe with spagetti and cheese.";
+        openAI.sendRequest(userQuestion);
+        String response = openAI.receiveResponse();
+
+        assertNotNull(response);
+
+        System.out.println("Response:" + response);
     }
 }
