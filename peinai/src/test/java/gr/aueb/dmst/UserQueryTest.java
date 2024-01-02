2b540d54fe97f86ec7b4ad9aa547206e9cc540d4
t8220128 <147388357+t8220128@users.noreply.github.com>
2024-01-03 01:16:45 +0200

    Update UserQueryTest.java
    changes of methods in the class

--- peinai/src/test/java/gr/aueb/dmst/UserQueryTest.java
+++ peinai/src/test/java/gr/aueb/dmst/UserQueryTest.java
@@ -1,91 +1,20 @@
 package gr.aueb.dmst;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
-
-public class UserQueryTest {
-
-    public UserQuery userQuery;
-
-    @BeforeEach
-    public void setUp() {
-        // Initialization of userQuery before each test
-        userQuery = new UserQuery("tell me a recipy that");
-    }
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.example.UserQuery;
+class UserQueryTest {
 
     @Test
-    public void testTrimQuery() {
-        userQuery.TrimQuery();
-        assertEquals("tell me a recipy that", userQuery.getQuery());
+    void modifyQuery() throws IOException {
+        // Initial query with inappropriate content
+        UserQuery userQuery = new UserQuery("Tell me an inappropriate recipy");
+
+        // Modify the query
+        String modifiedQuery = userQuery.modify();
+
+        // Ensure modifications are applied correctly
+        assertEquals("Tell me an inappropriate recipy", modifiedQuery);
     }
-
-    @Test
-    public void testLowerCaseQuery() {
-        userQuery.LowerCaseQuery();
-        assertEquals("tell me a recipy that", userQuery.getQuery());
-    }
-
-    @Test
-    public void testCapitalFirstLetter() {
-        userQuery.CapitalFirstLetter();
-        assertEquals("Tell me a recipy that", userQuery.getQuery());
-    }
-
-    @Test
-    public void testEmptyQuery() {
-        userQuery.setQuery("");
-        userQuery.EmptyQuery();
-        assertEquals("You have not entered a query. Please try again", userQuery.getQuery());
-    }
-
-    @Test
-    public void testremove_Newlines_Tabs() {
-        userQuery.remove_Newlines_Tabs();
-        assertEquals("tell me a recipy that", userQuery.getQuery());
-    }
-
-    @Test
-    public void testcheckLanguage() {
-        userQuery.setQuery("Καλημέρα");
-        userQuery.checkLanguage();
-        assertEquals("The query doesn't contain Latin characters. Please reenter your query in the English language", userQuery.getQuery());
-    }
-
-    @Test
-    public void testfoodContent() {
-        userQuery.setQuery("tell me a story");
-        userQuery.foodContent();
-        assertEquals("The query doesn't relate to food. Please try again.", userQuery.getQuery());
-    }
-    @Test
-    public void testModify() throws IOException {
-        // Δημιουργία αντικειμένου UserQuery με αρχικό κείμενο
-        UserQuery userQuery = new UserQuery("tell me a recipy that");
-
-        // Κλήση της μεθόδου modify()
-        String result = userQuery.modify();
-
-        // Έλεγχος των αναμενόμενων αλλαγών στο κείμενο
-        assertEquals("Tell me a recipy that", result);
-    }
-    @Test
-    public void testSetQuery() {
-        // Χρήση της μεθόδου setQuery
-        userQuery.setQuery("Αυτή είναι μια δοκιμαστική ερώτηση.");
-
-        // Επιβεβαίωση ότι η μέθοδος getQuery επιστρέφει τη σωστή τιμή
-        assertEquals("Αυτή είναι μια δοκιμαστική ερώτηση.", userQuery.getQuery());
-    }
-
-    @Test
-    public void testGetQuery() {
-        // Χρήση της μεθόδου setQuery για να ορίσετε μια ερώτηση
-        userQuery.setQuery("Αυτή είναι μια δοκιμαστική ερώτηση.");
-
-        // Επιβεβαίωση ότι η μέθοδος getQuery επιστρέφει τη σωστή τιμή
-        assertEquals("Αυτή είναι μια δοκιμαστική ερώτηση.", userQuery.getQuery());
-    }
-
 }
+
